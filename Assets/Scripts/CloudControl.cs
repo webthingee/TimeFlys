9853d728375b196ff3088b40df3c534e83dda554
Sean Lange <sean@webthingee.com>
2018-02-18 15:54:46 -0800

    clouds

---
+++ Assets/Scripts/CloudControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class CloudControl : MonoBehaviour 
+{
+	public float moveSpeed = 3f;
+
+    void Start () 
+    {
+        moveSpeed = Random.Range(.5f, 2f);
+
+        int rand = Random.Range(1,101);
+        if (rand % 2 == 0) 
+        {
+            GetComponent<SpriteRenderer>().sortingOrder = 2;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sortingOrder = -12;
+        }
+	}
+	
+	void Update () 
+    {
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/CloudSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour 
+{
+    public GameObject[] clouds;
+
+    public float minDropInterval = 2f;
+    public float maxDropInterval = 5f;
+	
+	void Start()
+    {
+        float sendCloudTime = Random.Range(minDropInterval, maxDropInterval);
+        StartCoroutine(DropBlock(sendCloudTime));
+    }   
+
+    IEnumerator DropBlock (float _wait)
+    {
+        Vector2 sendCloudFrom = SelectPosition();
+
+        int i = Random.Range(0, clouds.Length);
+        Instantiate(clouds[i], sendCloudFrom, Quaternion.identity, transform);
+
+        yield return new WaitForSeconds(_wait);
+        
+        float sendCloudTime = Random.Range(minDropInterval, maxDropInterval);
+        StartCoroutine(DropBlock(sendCloudTime));
+    }
+
+    Vector2 SelectPosition ()
+    {
+        float j = Random.Range(3f, 5f);
+        return new Vector2(transform.position.x, j);
+    }
+}
