b4c3ad9242540c7b0e41344a32c6bae32c99f1b6
Sean Lange <sean@webthingee.com>
2018-02-18 15:07:00 -0800

    updates

--- Assets/Scripts/FlyMovement.cs
+++ Assets/Scripts/FlyMovement.cs
@@ -8,15 +8,35 @@ public class FlyMovement : MonoBehaviour
 	public float moveSpeed = 3f;
 	public float animSpeed = 1f;
     public Animator animator;
+    public Vector2 goalPos;
+    public Vector2 newGoalPos;
 
     void Start () 
     {
+        animSpeed = Random.Range(.5f, 2f);
         animator.SetFloat("AnimSpeed", animSpeed);
 	}
 	
 	void Update () 
     {
         animator.SetFloat("AnimSpeed", animSpeed);
-        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        //transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        RandomFlyMovement();
 	}
+
+    public void RandomFlyMovement ()
+    {
+        /// MOVE
+        if ((Vector2)transform.position == goalPos)
+        {
+            float i = Random.Range(-8, 8);
+            float j = Random.Range(-4, 4);
+            goalPos = new Vector2(i,j);
+
+            animSpeed = Random.Range(.5f, 1.5f);
+            moveSpeed = Random.Range(.5f, 2f);
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, goalPos, moveSpeed * Time.deltaTime);
+    }
 }
